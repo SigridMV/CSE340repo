babe62da1bb87ba99b270003986042334866e3af
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2024-05-17 01:48:19 -0400

    W04 Prepare: Learning Activities ok

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation')
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
@@ -13,7 +14,9 @@ router.get(
 
 router.post(
   "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount)
-);
+)
 
 module.exports = router;
